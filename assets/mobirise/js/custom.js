a32feac5e8b0462434ae778601c7b38842034477
rootallecoindia <73353098+rootallecoindia@users.noreply.github.com>
2020-11-25 06:18:14 +0530

    Index

--- assets/mobirise/js/custom.js
+++ assets/mobirise/js/custom.js
@@ -11,7 +11,7 @@ $(function () {
 
 	setTimeout(function () {
 		$('.loader_bg').fadeToggle();
-	}, 2000);
+	}, 3500);
 
 	/* JQuery Menu
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
