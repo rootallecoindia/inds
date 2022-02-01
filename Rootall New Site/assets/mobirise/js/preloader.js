a8e0181749aa5b6e0494edd188f624ad5236ae7a
thedteam <thedteam18@gmail.com>
2022-02-01 15:27:34 +0530

    H

---
+++ Rootall New Site/assets/mobirise/js/preloader.js
@@ -0,0 +1,8 @@
+
+  /* Preloader
+  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
